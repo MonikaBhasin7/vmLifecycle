5c96db6e83d3ba346867eead511e6e73b857436e
MonikaBhasin7 <monikabhasin.sd17@gmail.com>
2021-02-17 09:02:17 +0530

    Activity Lifecycle

---
+++ app/src/main/java/com/hk/vmLifecycle/FirstActivity.kt
@@ -0,0 +1,121 @@
+package com.hk.vmLifecycle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.util.Log.INFO
+
+class FirstActivity : AppCompatActivity() {
+
+    /**
+     * It fires when the system first creates the activity. We must perform the basic application startup logic that should happen
+     * only once for the entire lifetime of the activity. Bind data to lists, associate the activity with the viewModel.
+     *
+     * When your activity is recreated after it was previously destroyed, you can recover your saved
+     * instance state from the Bundle that the system passes to your activity. Both the onCreate()
+     * and onRestoreInstanceState() callback methods receive the same Bundle that contains the
+     * instance state information.Because the onCreate() method is called whether the system is
+     * creating a new instance of your activity or recreating a previous one, you must check
+     * whether the state Bundle is null before you attempt to read it. If it is null, then the
+     * system is creating a new instance of the activity, instead of restoring a previous one that was destroyed.
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        println("OnCreate")
+        println("The saved Instance is - ${savedInstanceState?.getString("vishMon")}")
+        setContentView(R.layout.activity_first)
+    }
+
+    /**
+     * Instead of restoring the state during onCreate() you may choose to implement onRestoreInstanceState(),
+     * which the system calls after the onStart() method. The system calls onRestoreInstanceState()
+     * only if there is a saved state to restore, so you do not need to check whether the Bundle is null:
+     */
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        println("OnRestoreStateInstanceState")
+    }
+
+    /**
+     * As your activity begins to stop, the system calls the onSaveInstanceState() method so your activity
+     * can save state information to an instance state bundle. The default implementation of this method
+     * saves transient information about the state of the activity's view hierarchy, such as the text in
+     * an EditText widget or the scroll position of a ListView widget.
+     */
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        println("onSaveInstanceState")
+        outState.run {
+            putString("vishMon", "vvvmmm")
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        println("onStart")
+    }
+
+    /**
+     * This is the state in which the app interacts with the user.
+     * The app stays in this state until something happens to take focus away from the app.
+     * When an interruptive event occurs, the activity enters the Paused state, and the system invokes the onPause() callback.
+     */
+    override fun onResume() {
+        super.onResume()
+        println("onResume")
+    }
+
+    /**
+     * The system calls this method as the first indication that the user is leaving your activity
+     * (though it does not always mean the activity is being destroyed); it indicates that the activity
+     * is no longer in the foreground (though it may still be visible if the user is in multi-window mode).
+     * You can also use the onPause() method to release system resources, handles to sensors (like GPS),
+     * or any resources that may affect battery life while your activity is paused and the user does not need them.
+     * However, as mentioned above in the onResume() section, a Paused activity may still be fully visible
+     * if in multi-window mode. As such, you should consider using onStop() instead of onPause()
+     * to fully release or adjust UI-related resources and operations to better support multi-window mode.
+     * Completion of the onPause() method does not mean that the activity leaves the Paused state.
+     * Rather, the activity remains in this state until either the activity resumes or becomes completely invisible
+     * to the user. If the activity resumes, the system once again invokes the onResume() callback.
+     * If the activity returns from the Paused state to the Resumed state, the system keeps the Activity
+     * instance resident in memory, recalling that instance when the system invokes onResume().
+     * In this scenario, you don’t need to re-initialize components that were created during any
+     * of the callback methods leading up to the Resumed state.
+     */
+    override fun onPause() {
+        super.onPause()
+        println("onPause")
+    }
+
+    /**
+     * n the onStop() method, the app should release or adjust resources that are not needed while
+     * the app is not visible to the user. For example, your app might pause animations or switch
+     * from fine-grained to coarse-grained location updates. Using onStop() instead of onPause()
+     * ensures that UI-related work continues, even when the user is viewing your activity in multi-window mode.
+     * ou should also use onStop() to perform relatively CPU-intensive shutdown operations.
+     * For example, if you can't find a more opportune time to save information to a database, you might do so during onStop().
+     * When your activity enters the Stopped state, the Activity object is kept resident in memory: It maintains
+     * all state and member information, but is not attached to the window manager. When the activity resumes,
+     * the activity recalls this information. You don’t need to re-initialize components that were created
+     * during any of the callback methods leading up to the Resumed state. The system also keeps
+     * track of the current state for each View object in the layout, so if the user entered text into an
+     * EditText widget, that content is retained so you don't need to save and restore it.
+     * From the Stopped state, the activity either comes back to interact with the user, or the activity is finished running and goes away.
+     * If the activity comes back, the system invokes onRestart(). If the Activity is finished running, the system calls onDestroy()
+     */
+    override fun onStop() {
+        super.onStop()
+        println("onStop")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        println("onRestart")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        println("onDestroy")
+    }
+}
---
+++ app/src/main/java/com/hk/vmLifecycle/readme.kt
@@ -0,0 +1,18 @@
+package com.hk.vmLifecycle
+
+/*2021-02-17 08:27:43.201 399-399/com.hk.vmLifecycle I/System.out: OnCreate
+2021-02-17 08:27:43.329 399-399/com.hk.vmLifecycle I/System.out: onStart
+2021-02-17 08:27:43.339 399-399/com.hk.vmLifecycle I/System.out: onResume
+2021-02-17 08:28:29.780 399-399/com.hk.vmLifecycle I/System.out: onPause
+2021-02-17 08:28:29.832 399-399/com.hk.vmLifecycle I/System.out: onSaveInstanceState
+2021-02-17 08:28:29.834 399-399/com.hk.vmLifecycle I/System.out: onStop
+2021-02-17 08:28:47.916 399-399/com.hk.vmLifecycle I/System.out: onRestart
+2021-02-17 08:28:47.920 399-399/com.hk.vmLifecycle I/System.out: onStart
+2021-02-17 08:28:47.922 399-399/com.hk.vmLifecycle I/System.out: onResume
+2021-02-17 08:28:52.475 399-399/com.hk.vmLifecycle I/System.out: onPause
+2021-02-17 08:28:52.715 399-399/com.hk.vmLifecycle I/System.out: onStop
+2021-02-17 08:28:52.717 399-399/com.hk.vmLifecycle I/System.out: onDestroy
+2021-02-17 08:29:01.640 399-399/com.hk.vmLifecycle I/System.out: OnCreate
+2021-02-17 08:29:01.671 399-399/com.hk.vmLifecycle I/System.out: onStart
+2021-02-17 08:29:01.676 399-399/com.hk.vmLifecycle I/System.out: onResume
+*/
